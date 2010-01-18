45135ea056d67db6b474f1c91432d0bfb9aec725
dalloliogm <dalloliogm@gmail.com>
2010-01-18 17:41:54 +0100

    a function to launch a query on google and print the results

--- src/launch_query.py
+++ src/launch_query.py
@@ -2,8 +2,30 @@
 """
 Given a google query with the -q option, it launches it to google and returns the results.
 """
+import simplejson
+import urllib
+import urllib2
 
 
+
+def launch_query(query, start):
+    """
+    Execute a query on google, getting exactly 4 results.
+    
+    It appears that you can't get more than 4 results at a time with the google ajax APIs.
+    To get more results for a query, you must launch this function multiple times changing the 'start' parameters.
+    """
+    baseurl = 'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&%s'
+    parameters = {'q': query, 'start': start}
+    queryurl = baseurl % urllib.urlencode(parameters)
+#    print queryurl
+
+    opener = urllib2.build_opener()
+    f = opener.open(queryurl)
+
+    response = simplejson.load(f)
+    for result in response['responseData']['results']:
+        print result['title']
 
 
 
@@ -14,6 +36,8 @@ if __name__ == '__main__':
     parser.add_argument('--query', metavar='query', type=str, nargs='+')
 
     args = parser.parse_args()
-    print args.query
-    print dir(args)
+    query = args.query
+    print query
+    launch_query(query, 0)
+    launch_query(query, 4)
 
