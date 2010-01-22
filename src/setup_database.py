6046e65ad003dc0e13f2e8d8448840bc08dd3f02
dalloliogm <dalloliogm@gmail.com>
2010-01-22 20:34:36 +0100

    setup database script

---
+++ src/setup_database.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+"""
+"""
+
+from elixir import setup_all, create_all, session
+from schema import *
+
+connection_line = 'sqllite:///file:querycache.sqllite'
+
+
+
+
+
+if __name__ == '__main__':
+    pass
+
