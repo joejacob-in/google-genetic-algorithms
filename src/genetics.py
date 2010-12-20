9d0657a70832e04e45c0146ce5d5e304538dff02
Giovanni Marco Dall'Olio <dalloliogm@gmail.com>
2010-12-20 18:19:08 +0100

    trying to integrate the tutorial on pyevolve at http://pyevolve.sourceforge.net/getstarted.html

--- src/genetics.py
+++ src/genetics.py
@@ -5,6 +5,8 @@ main pipeline for the Genetic Algorithm
 
 
 import pyevolve as pv
+from pyevolve import GSimpleGA
+from pyevolve import G1DList
 from launch_query import launch_query, print_fancy_results
 import random
 import re
@@ -31,14 +33,35 @@ def eval_func(chromosome):
     >>> import random
     >>> chromosome = [chr(random.randrange(50,150)) for x in xrange(50)]
     """
+    print "CHROMOSOME:", chromosome
     chromosome = ''.join(chromosome)
     chromosome = notalpha_regex.sub(' ', chromosome)
-    print chromosome
-    results = launch_query(chromosome)
-    print results
-    print_fancy_results(results)
+##    print chromosome
+#    results = launch_query(chromosome)
+##    print results
+##    print_fancy_results(results)
 
-    return results
+    score = random.randrange(100)
+
+#    if not results['responseData']['cursor'].has_key('estimatedResultCount'):
+#        score = 0
+#    else:
+#        score = results['responseData']['cursor']['estimatedResultCount']
+#
+#    print chromosome
+#    print score
+    return score
+
+
+def run():
+    """
+    """
+    genome = G1DList.G1DList(20)
+    genome.evaluator.set(eval_func)
+    ga = GSimpleGA.GSimpleGA(genome)
+    ga.evolve(freq_stats=10)
+    print ga.bestIndividual()
+
 
 
 def test_evalWithRandomValues(seed):
@@ -48,7 +71,7 @@ def test_evalWithRandomValues(seed):
     """
     random.seed(seed)
     chromosome = [chr(random.randrange(ascii_start, ascii_stop)) for x in xrange(seq_length)]
-    print chromosome
+#    print chromosome
     res = eval_func(chromosome)
     return res
 
@@ -66,6 +89,7 @@ def test_normalresults():
     return results
 
 if __name__ == '__main__':
-    nores = test_noresults()
-    splicing = test_normalresults()
+#    nores = test_noresults()
+#    splicing = test_normalresults()
 
+    run()
