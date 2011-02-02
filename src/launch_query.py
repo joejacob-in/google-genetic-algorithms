d5d3215aa36d6dba3fc895df8a98ecec50a566a9
Giovanni Marco Dall'Olio <dalloliogm@gmail.com>
2011-02-02 15:11:13 +0100

    Fix: handling empty results
    TODO: check why empty results are returned for some queries

--- src/launch_query.py
+++ src/launch_query.py
@@ -39,8 +39,8 @@ def launch_query(query, key):
     baseurl = 'https://www.googleapis.com/customsearch/v1?%s'
 #    parameters = {'q': query, 'start': start, 'key': key}
     # parameters needed: q, cx, key
-#    parameters = {'q': query, 'start': start, 'key': key, 'cx': '017576662512468239146:omuauf_lfve'}
-    parameters = {'q': query, 'cx': '017576662512468239146:omuauf_lfve', 'key': key}
+    parameters = {'q': query, 'cx': '017576662512468239146', 'key': key}
+#    parameters = {'q': query, 'cx': '017576662512468239146:omuauf_lfve', 'key': key}
     queryurl = baseurl % urllib.urlencode(parameters)
     logging.debug(queryurl)
 
@@ -62,9 +62,12 @@ def print_fancy_results(response):
     logging.debug(response)
     number_results = response['queries']['request'][0]['totalResults']
     print term.render('${RED}Showing the first 10 results out of %s:${NORMAL}' % number_results)
-    for result in response['items']:
-        output = '- ${BLUE}' + result['title'] + '${NORMAL} (' + result['link'] + ')'
-        print term.render("%s" % output)
+    if number_results == 0:
+        print 'no results'
+    else:
+        for result in response['items']:
+            output = '- ${BLUE}' + result['title'] + '${NORMAL} (' + result['link'] + ')'
+            print term.render("%s" % output)
         
 
 
