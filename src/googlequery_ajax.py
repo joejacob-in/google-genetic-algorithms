e5cf8c7cf128dd5e894581ac090772e0ef7b8b86
Giovanni Marco Dall'Olio <dalloliogm@gmail.com>
2011-02-04 18:51:49 +0100

    Refactore: Implemented a command line interface for google_ajax query script

--- src/googlequery_ajax.py
+++ src/googlequery_ajax.py
@@ -7,25 +7,47 @@ import simplejson
 import urllib
 import urllib2
 import time
+import argparse
+import logging
 
 baseurl = 'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&%s'
 
 
-parameters = {'q': 'alternative splicing', 'start': 4}
+#parameters = {'q': 'alternative splicing', 'start': 4}
 #query = urllib.urlencode({'q': 'alternative splicing'})
-queryurl = baseurl % urllib.urlencode(parameters)
-print queryurl
+def launch_query_ajax(query, start=0):
+	"""
+	Launch a query through the ajax APIs
+	"""
+	parameters = {'q': query, 'start':start}
+	queryurl = baseurl % urllib.urlencode(parameters)
+	print queryurl
 
-opener = urllib2.build_opener()
-f = opener.open(queryurl)
+	# TODO: implement a UserAgent and possibly use WebBrowser
+	opener = urllib2.build_opener()
+	f = opener.open(queryurl)
 
-response = simplejson.load(f)
-#print response
+	response = simplejson.load(f)
+	#print response
 
-for result in response['responseData']['results']:
-    print result['title']
-time.sleep(5)
+	for result in response['responseData']['results']:
+	    print result['title']
+	logging.debug(result)
+	time.sleep(1)
+
 
 if __name__ == '__main__':
-    pass
+    parser = argparse.ArgumentParser(description = __doc__)
+    parser.add_argument('-q', '--query', metavar='query', type=str, nargs='+', help='query', required=True)
+    parser.add_argument('-d', '--debug', action='store_true', help='print debugging messages')
+
+    args = parser.parse_args()
+    if args.debug is True:
+        logging.basicConfig(level=logging.DEBUG)
+    query = args.query
+    print query
+
+    results1 = launch_query_ajax(query, start=0)
+#    print_fancy_results(results1)
+
 
