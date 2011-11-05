cde7d1a0c8f3cdbdb104a595c4a268c8fe63cbaa
Giovanni Marco Dall'Olio <dalloliogm@gmail.com>
2011-11-05 23:50:32 +0100

    FIX: working on mutator function

--- src/GoogleQueryGenome.py
+++ src/GoogleQueryGenome.py
@@ -66,39 +66,62 @@ def googleQueryInitializer(genome, **args):
     """
     initialize google query genome
     """
-    chr1 = [chr(random.randint(0, 255)) for n in xrange(genome.getParam("seqlength"))]
-    chr2 = [chr(random.randint(0, 255)) for n in xrange(genome.getParam("seqlength"))]
+    chr1 = [chr(random.randint(30, 126)) for n in xrange(genome.getParam("seqlength"))]
+    chr2 = [chr(random.randint(30, 126)) for n in xrange(genome.getParam("seqlength"))]
+#    chr1 = [chr(random.randint(0, 255)) for n in xrange(genome.getParam("seqlength"))]
+#    chr2 = [chr(random.randint(0, 255)) for n in xrange(genome.getParam("seqlength"))]
     genome.genomeList = [chr1, chr2]
 
 
 def googleQueryMutator(genome, **args):
-   """ The mutator of G1DList, Swap Mutator
+   """ The mutator of GoogleQueryChromosome
    
-   .. note:: this mutator is :term:`Data Type Independent`
-
    """
    
+   print "Mutating genome", genome
    if args["pmut"] <= 0.0: return 0
-   height, width = genome.getSize()
+#   height, width = genome.getParam('seqlength')
+   height = 2
+   width = genome.getParam('seqlength')
    elements = height * width
 
    mutations = args["pmut"] * elements
 
    if mutations < 1.0:
       mutations = 0
-      for i in xrange(height):
-         for j in xrange(width):
-            if Util.randomFlipCoin(args["pmut"]):
-               index_b = (rand_randint(0, height-1), rand_randint(0, width-1))
-               Util.list2DSwapElement(genome.genomeList, (i,j), index_b)
-               mutations+=1
+   for i in xrange(height):
+      for j in xrange(width):
+         if Util.randomFlipCoin(args["pmut"]):
+             index_b = (random.randint(0, height-1), random.randint(0, width-1))
+             list2DSwapElement(genome.genomeList, (i,j), index_b)
+             mutations+=1
    else:
       for it in xrange(int(round(mutations))):
-         index_a = (rand_randint(0, height-1), rand_randint(0, width-1))
-         index_b = (rand_randint(0, height-1), rand_randint(0, width-1))
+         index_a = (random.randint(0, height-1), random.randint(0, width-1))
+         index_b = (random.randint(0, height-1), random.randint(0, width-1))
          Util.list2DSwapElement(genome.genomeList, index_a, index_b)
 
    return int(mutations)
+
+def list2DSwapElement(lst, indexa, indexb):
+   """ Swaps elements A and B in a 2D list (matrix).
+
+   Example:
+      >>> l = [ [1,2,3], [4,5,6] ] 
+      >>> Util.list2DSwapElement(l, (0,1), (1,1) )
+      >>> l
+      [[1, 5, 3], [4, 2, 6]]
+
+   :param lst: the list
+   :param indexa: the swap element A
+   :param indexb: the swap element B
+   :rtype: None
+
+   """
+   temp = lst[indexa[0]][indexa[1]]
+   lst[indexa[0]][indexa[1]] = lst[indexb[0]][indexb[1]]
+   lst[indexb[0]][indexb[1]] = temp
+
 
 def test():
     pass
